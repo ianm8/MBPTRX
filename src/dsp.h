b4733b2b2477804b6ec3cd709e7d06d3a70598a6
Ian Mitchell VK7IAN <ian.mitchell@utas.edu.au>
2025-09-07 17:52:18 +1000

    Version 0.8.240

---
+++ src/dsp.h
@@ -0,0 +1,308 @@
+#ifndef DSP_H
+#define DSP_H
+
+#include "filter.h"
+#include "util.h"
+
+namespace DSP
+{
+  volatile static float agc_peak = 0.0f;
+
+  static const int16_t __not_in_flash_func(agc)(const float in)
+  {
+    // limit gain to max of 40 (32db)
+    static const float max_gain = 40.0f;
+    // decay about 10dB per second
+    static const float k = 0.99996f;
+
+    const float magnitude = fabsf(in);
+    if (magnitude>agc_peak)
+    {
+      agc_peak = magnitude;
+    }
+    else
+    {
+      agc_peak *= k;
+    }
+
+    // trap issues with low values
+    if (agc_peak<1.0f) return (int16_t)(in * max_gain);
+
+    // set maximum gain possible for 12 bit DAC
+    const float m = 2047.0f/agc_peak;
+    return (int16_t)(in*fminf(m,max_gain));
+  }
+
+  static const uint8_t __not_in_flash_func(smeter)(void)
+  {
+    // S9 = -73dBm = 141uV PP
+    // need to return 10 for S9
+    static const float S0_sig = 100.0f;
+    static const float S9_sig = 300.0f;
+    static const float S9p_sig = 8192.0f;
+    static const uint32_t S9_from_min = (uint32_t)(log10f(S0_sig) * 1024.0f);
+    static const uint32_t S9_from_max = (uint32_t)(log10f(S9_sig) * 1024.0f);
+    static const uint32_t S9_min = 0ul;
+    static const uint32_t S9_max = 10ul;
+    static const uint32_t S9p_from_min = (uint32_t)(log10f(S9_sig) * 1024.0f);
+    static const uint32_t S9p_from_max = (uint32_t)(log10f(S9p_sig) * 1024.0f);
+    static const uint32_t S9p_min = 11ul;
+    static const uint32_t S9p_max = 15ul;
+    if (agc_peak<1.0f)
+    {
+      return 0u;
+    }
+    const uint32_t log_peak = (uint32_t)(log10f(agc_peak) * 1024.0f);
+    if (agc_peak>S9_sig)
+    {
+      return (uint8_t)UTIL::map(log_peak,S9p_from_min,S9p_from_max,S9p_min,S9p_max);
+    }
+    return (uint8_t)UTIL::map(log_peak,S9_from_min,S9_from_max,S9_min,S9_max);
+  }
+
+  static const int16_t __not_in_flash_func(process_ssb_ORG)(const int16_t in_i,const int16_t in_q,const uint32_t jnr_level,const uint8_t bw)
+  {
+    // remove DC
+    const float ii = FILTER::dc1f((float)in_i / 32768.0f);
+    const float qq = FILTER::dc2f((float)in_q / 32768.0f);
+
+    // phase shift IQ +/- 45
+    const float p45 = FILTER::fap1f(ii);
+    const float n45 = FILTER::fap2f(qq);
+
+    // reject image
+    const float ssb = p45 - n45;
+
+    // LPF
+    const float audio_lpf = FILTER::bwf[bw](ssb);
+
+    // HPF
+    const float audio_raw = FILTER::hpf_300f(audio_lpf);
+
+    // JNR
+    const float audio_out = FILTER::jnr(audio_raw,jnr_level);
+
+    // AGC returns 12 bit value
+    return agc(audio_out * 32768.0f);
+  }
+
+  static const int16_t __not_in_flash_func(process_ssb_LPF)(const int16_t in_i,const int16_t in_q,const uint32_t jnr_level,const uint8_t bw)
+  {
+    // remove DC
+    const float ii_ac = FILTER::dc1f((float)in_i / 32768.0f);
+    const float qq_ac = FILTER::dc2f((float)in_q / 32768.0f);
+
+    // remove first image
+    const float ii = FILTER::lpf_fs4f_rx1(ii_ac);
+    const float qq = FILTER::lpf_fs4f_rx2(qq_ac);
+
+    // 2 bit local oscillator 
+    volatile static struct {uint32_t lo:2;} quad = {0};
+
+    // reject image (quadrature LO at FS/4 and difference)
+    // USB - left
+    //const float ssb = ((float[]){qq,ii,-qq,-ii})[quad.lo++];
+
+    // LSB - right
+    const float ssb = ((float[]){ii,qq,-ii,-qq})[quad.lo++];
+
+    // LPF
+    const float audio_lpf = FILTER::bwf[bw](ssb);
+
+    // JNR
+    const float audio_out = FILTER::jnr(audio_lpf,jnr_level);
+
+    // AGC returns 12 bit value
+    return agc(audio_out * 32768.0f);
+  }
+
+  static const int16_t __not_in_flash_func(process_ssb_FS4)(const int16_t in_i,const int16_t in_q,const uint32_t jnr_level,const uint8_t bw)
+  {
+    // LSB - left @FS/4 spectrum
+    // USB - right @FS/4 spectrum
+
+    // remove DC
+    const float ii_ac = FILTER::dc1f((float)in_i / 32768.0f);
+    const float qq_ac = FILTER::dc2f((float)in_q / 32768.0f);
+
+    // remove first image
+    const float ii = FILTER::hpf_fs4f_rx1(ii_ac);
+    const float qq = FILTER::hpf_fs4f_rx2(qq_ac);
+
+    // 2 bit local oscillator 
+    volatile static struct {uint32_t lo:2;} quad = {0};
+
+    // reject image (quadrature LO at FS/4 and difference)
+    const float ssb = ((float[]){qq,ii,-qq,-ii})[quad.lo++];
+
+    // LPF
+    const float audio_lpf = FILTER::bwf[bw](ssb);
+
+    // JNR
+    const float audio_out = FILTER::jnr(audio_lpf,jnr_level);
+
+    // AGC returns 12 bit value
+    return agc(audio_out * 32768.0f);
+  }
+
+  static const int16_t __not_in_flash_func(process_ssb_FS8)(const int16_t in_i,const int16_t in_q,const uint32_t jnr_level,const uint8_t bw)
+  {
+    // LSB - left spectrum
+    // USB - right spectrum
+    volatile static uint8_t lo = 0;
+
+    // remove DC
+    const float ii_ac = FILTER::dc1f((float)in_i / 32768.0f);
+    const float qq_ac = FILTER::dc2f((float)in_q / 32768.0f);
+
+    // remove first image
+    const float ii = FILTER::hpf_fs8f_rx1(ii_ac);
+    const float qq = FILTER::hpf_fs8f_rx2(qq_ac);
+
+    // reject image (quadrature LO and difference)
+    const uint8_t I_index = lo;
+    const uint8_t Q_index = lo+2;
+    const float loI = UTIL::s8[I_index];
+    const float loQ = UTIL::s8[Q_index];
+    const float ssb = ii*loI + qq*loQ;
+    lo++;
+
+    // LPF
+    const float audio_lpf = FILTER::bwf[bw](ssb);
+
+    // JNR
+    const float audio_out = FILTER::jnr(audio_lpf,jnr_level);
+
+    // AGC returns 12 bit value
+    return agc(audio_out * 32768.0f);
+  }
+
+  static const int16_t __not_in_flash_func(process_ssb)(const int16_t in_i,const int16_t in_q,const uint32_t jnr_level,const uint8_t bw)
+  {
+    // quadrature mixer
+
+    // quadrature LO = 3906 (quadrature: 31250/8/4 = 2)
+    // quadrature LO = 1953 (quadrature: 31250/16/4 = 4)
+    // quadrature LO = 977 (quadrature: 31250/32/4 = 8)
+    // quadrature LO = 488 (quadrature: 31250/64/4 = 16)
+    static const uint8_t PI2 = 16u;
+    volatile static uint8_t lo = 0;
+    const uint8_t I_index = lo;
+    const uint8_t Q_index = lo + PI2;
+    const float loI = UTIL::s64[I_index];
+    const float loQ = UTIL::s64[Q_index];
+    lo++;
+
+    // remove DC
+    const float ii = FILTER::dc1f((float)in_i / 32768.0f);
+    const float qq = FILTER::dc2f((float)in_q / 32768.0f);
+
+    // quadrature down-convert
+    const float a = ii * loQ;
+    const float b = qq * loI;
+    const float c = qq * loQ;
+    const float d = ii * loI;
+    const float mxI = a - b;
+    const float mxQ = c + d;
+
+    // phase shift IQ +/- 45
+    const float p45 = FILTER::fap1f(mxI);
+    const float n45 = FILTER::fap2f(mxQ);
+
+    // reject image
+    const float ssb = p45 - n45;
+
+    // LPF
+    const float audio_lpf = FILTER::bwf[bw](ssb);
+
+    // HPF
+    const float audio_raw = FILTER::hpf_200f(audio_lpf);
+
+    // JNR
+    const float audio_out = FILTER::jnr(audio_raw,jnr_level);
+    //const float audio_out = FILTER::jnr(audio_lpf,jnr_level);
+
+    // AGC returns 12 bit value
+    return agc(audio_out * 32768.0f);
+  }
+
+  static const int16_t __not_in_flash_func(process_cw)(const int16_t in_i,const int16_t in_q)
+  {
+    // remove DC
+    const float ii = FILTER::dc1f((float)in_i / 32768.0f);
+    const float qq = FILTER::dc2f((float)in_q / 32768.0f);
+
+    // phase shift IQ +/- 45
+    const float p45 = FILTER::fap1f(ii);
+    const float n45 = FILTER::fap2f(qq);
+
+    // reject image
+    const float ssb = p45 - n45;
+
+    // BPF for CW
+    const float audio_out = FILTER::bpf_700f(ssb);
+
+    // AGC returns 12 bit value
+    return agc(audio_out * 32768.0f);
+  }
+
+  static const uint32_t __not_in_flash_func(get_mic_peak_level)(const int16_t mic_in)
+  {
+    static const uint32_t MIC_LEVEL_DECAY_RATE = 50ul;
+    static const uint32_t MIC_LEVEL_HANG_TIME = 500ul;
+    static uint32_t mic_peak_level = 0;
+    static uint32_t mic_level_update = 0;
+    static uint32_t mic_hangtime_update = 0;
+    const uint32_t now = millis();
+    const uint32_t mic_level = abs(mic_in)>>5;
+    if (mic_level>mic_peak_level)
+    {
+      mic_peak_level = mic_level;
+      mic_level_update = now + MIC_LEVEL_DECAY_RATE;
+      mic_hangtime_update = now + MIC_LEVEL_HANG_TIME;
+    }
+    else
+    {
+      if (now>mic_hangtime_update)
+      {
+        if (now>mic_level_update)
+        {
+          if (mic_peak_level) mic_peak_level--;
+          mic_level_update = now + MIC_LEVEL_DECAY_RATE;
+        }
+      }
+    }
+    return mic_peak_level;
+  }
+
+  static void __not_in_flash_func(cessb)(float& ii, float& qq)
+  {
+    const float mag_raw = sqrtf(ii*ii + qq*qq);
+    const float mag_max = fmaxf(mag_raw, 1.0f);
+    ii = FILTER::lpf_2600if_tx(ii / mag_max);
+    qq = FILTER::lpf_2600qf_tx(qq / mag_max);
+  }
+
+  const void __not_in_flash_func(process_mic)(const int16_t s,int16_t &out_i,int16_t &out_q,const float mic_gain,const bool cessb_on)
+  {
+    // input is 12 bits
+    // convert to float
+    // remove Mic DC
+    // 2400 LPF 
+    // phase shift I
+    // phase shift Q
+    // convert to int
+    // output is 10 bits
+    const float ac_sig = FILTER::dcf(((float)s)*(1.0f/2048.0f));
+    const float mic_sig = FILTER::lpf_2600f_tx(ac_sig);
+    const float ii1 = FILTER::fap1f(mic_sig);
+    const float qq1 = FILTER::fap2f(mic_sig);
+    float ii2 = ii1 * mic_gain;
+    float qq2 = qq1 * mic_gain;
+    if (cessb_on) cessb(ii2,qq2);
+    out_i = (int16_t)(ii2 * 512.0f);
+    out_q = (int16_t)(qq2 * 512.0f);
+  }
+}
+
+#endif
---
+++ src/menu.h
@@ -0,0 +1,335 @@
+#ifndef MENU_H
+#define MENU_H
+
+/*
+
+ Step
+  10
+  100
+  500
+  1000
+  10000
+  Exit
+
+ Mode
+   LSB
+   USB
+   CWL
+   CWU
+   Auto
+   Exit
+
+  Band
+    80M
+    40M
+    ...
+    Exit
+
+  ...
+
+  Exit
+    Exit
+
+*/
+
+#define NUM_MENU_ITEMS 12U
+#define NUM_MENU_OPTIONS 10U
+
+enum menu_top_t
+{
+  MENU_BAND,
+  MENU_STEP,
+  MENU_MODE,
+  MENU_CW_SPEED,
+  MENU_SIDETONE,
+  MENU_SIDETONE_LEVEL,
+  MENU_SPECTRUM_TYPE,
+  MENU_JNR,
+  MENU_MIC,
+  MENU_CESSB,
+  MENU_GAUSSIAN,
+  MENU_EXIT
+};
+
+enum option_value_t
+{
+  OPTION_STEP_10,
+  OPTION_STEP_100,
+  OPTION_STEP_500,
+  OPTION_STEP_1000,
+  OPTION_STEP_5000,
+  OPTION_STEP_10000,
+  OPTION_STEP_100000,
+  OPTION_MODE_USB,
+  OPTION_MODE_LSB,
+  OPTION_MODE_CWL,
+  OPTION_MODE_CWU,
+  OPTION_MODE_AUTO,
+  OPTION_BAND_80M,
+  OPTION_BAND_40M,
+  OPTION_BAND_30M,
+  OPTION_BAND_20M,
+  OPTION_BAND_17M,
+  OPTION_BAND_15M,
+  OPTION_BAND_12M,
+  OPTION_BAND_10M,
+  OPTION_BAND_SWL,
+  OPTION_GAUSSIAN_ON,
+  OPTION_GAUSSIAN_OFF,
+  OPTION_CW_SPEED_10,
+  OPTION_CW_SPEED_15,
+  OPTION_CW_SPEED_20,
+  OPTION_CW_SPEED_25,
+  OPTION_CW_SPEED_30,
+  OPTION_SIDETONE_500,
+  OPTION_SIDETONE_550,
+  OPTION_SIDETONE_600,
+  OPTION_SIDETONE_650,
+  OPTION_SIDETONE_700,
+  OPTION_SIDETONE_750,
+  OPTION_SIDETONE_800,
+  OPTION_SIDETONE_850,
+  OPTION_SIDETONE_LOW,
+  OPTION_SIDETONE_MED,
+  OPTION_SIDETONE_HI,
+  OPTION_SPECTRUM_WIND,
+  OPTION_SPECTRUM_GRASS,
+  OPTION_JNR_OFF,
+  OPTION_JNR_LEVEL1,
+  OPTION_JNR_LEVEL2,
+  OPTION_JNR_LEVEL3,
+  OPTION_MIC_25,
+  OPTION_MIC_50,
+  OPTION_MIC_75,
+  OPTION_MIC_100,
+  OPTION_MIC_125,
+  OPTION_MIC_150,
+  OPTION_MIC_175,
+  OPTION_MIC_200,
+  OPTION_CESSB_ON,
+  OPTION_CESSB_OFF,
+  OPTION_NONE,
+  OPTION_EXIT
+};
+
+struct options_t
+{
+  option_value_t option_value;
+  const char* option_name;
+};
+
+volatile static struct
+{
+  const menu_top_t menu_value;
+  const char *menu_name;
+  const uint8_t num_options;
+  const options_t options[NUM_MENU_OPTIONS];
+}
+menu_options[NUM_MENU_ITEMS] =
+{
+  {
+    MENU_BAND,
+    "Band",
+    10U,
+    {
+      {OPTION_BAND_80M,"80M"},
+      {OPTION_BAND_40M,"40M"},
+      {OPTION_BAND_30M,"30M"},
+      {OPTION_BAND_20M,"20M"},
+      {OPTION_BAND_17M,"17M"},
+      {OPTION_BAND_15M,"15M"},
+      {OPTION_BAND_12M,"12M"},
+      {OPTION_BAND_10M,"10M"},
+      {OPTION_BAND_SWL,"SWL"},
+      {OPTION_EXIT,"Exit"}
+    }
+  },
+  {
+    MENU_STEP,
+    "Step",
+    8U,
+    {
+      {OPTION_STEP_10,"10"},
+      {OPTION_STEP_100,"100"},
+      {OPTION_STEP_500,"500"},
+      {OPTION_STEP_1000,"1000"},
+      {OPTION_STEP_5000,"5000"},
+      {OPTION_STEP_10000,"10000"},
+      {OPTION_STEP_100000,"100000"},
+      {OPTION_EXIT,"Exit"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"}
+    }
+  },
+  {
+    MENU_MODE,
+    "Mode",
+    6U,
+    {
+      {OPTION_MODE_LSB,"LSB"},
+      {OPTION_MODE_USB,"USB"},
+      {OPTION_MODE_CWL,"CWL"},
+      {OPTION_MODE_CWU,"CWU"},
+      {OPTION_MODE_AUTO,"AUTO"},
+      {OPTION_EXIT,"Exit"},
+      {OPTION_EXIT,"Exit"},
+      {OPTION_EXIT,"Exit"},
+      {OPTION_EXIT,"Exit"},
+      {OPTION_EXIT,"Exit"}
+    }
+  },
+  {
+    MENU_JNR,
+    "JNR",
+    4U,
+    {
+      {OPTION_JNR_LEVEL1,"Level 1"},
+      {OPTION_JNR_LEVEL2,"Level 2"},
+      {OPTION_JNR_LEVEL3,"Level 3"},
+      {OPTION_JNR_OFF,"Off"},
+      {OPTION_EXIT,"Exit"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"}
+    }
+  },
+  {
+    MENU_CW_SPEED,
+    "CW Speed",
+    6U,
+    {
+      {OPTION_CW_SPEED_10,"10 WPM"},
+      {OPTION_CW_SPEED_15,"15 WPM"},
+      {OPTION_CW_SPEED_20,"20 WPM"},
+      {OPTION_CW_SPEED_25,"25 WPM"},
+      {OPTION_CW_SPEED_30,"30 WPM"},
+      {OPTION_EXIT,"Exit"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"}
+    }
+  },
+  {
+    MENU_SIDETONE,
+    "CW Tone",
+    9U,
+    {
+      {OPTION_SIDETONE_500,"500 Hz"},
+      {OPTION_SIDETONE_550,"550 Hz"},
+      {OPTION_SIDETONE_600,"600 Hz"},
+      {OPTION_SIDETONE_650,"650 Hz"},
+      {OPTION_SIDETONE_700,"700 Hz"},
+      {OPTION_SIDETONE_750,"750 Hz"},
+      {OPTION_SIDETONE_800,"800 Hz"},
+      {OPTION_SIDETONE_850,"850 Hz"},
+      {OPTION_EXIT,"Exit"},
+      {OPTION_NONE,"None"}
+    }
+  },
+  {
+    MENU_SIDETONE_LEVEL,
+    "CW Level",
+    4U,
+    {
+      {OPTION_SIDETONE_LOW,"Low"},
+      {OPTION_SIDETONE_MED,"Medium"},
+      {OPTION_SIDETONE_HI,"High"},
+      {OPTION_EXIT,"Exit"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"}
+    }
+  },
+  {
+    MENU_SPECTRUM_TYPE,
+    "Spectrum",
+    3U,
+    {
+      {OPTION_SPECTRUM_WIND,"Wind"},
+      {OPTION_SPECTRUM_GRASS,"Grass"},
+      {OPTION_EXIT,"Exit"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"}
+    }
+  },
+  {
+    MENU_MIC,
+    "Mic Gain",
+    9U,
+    {
+      {OPTION_MIC_25,"Gain 25%"},
+      {OPTION_MIC_50,"Gain 50%"},
+      {OPTION_MIC_75,"Gain 75%"},
+      {OPTION_MIC_100,"Gain 100%"},
+      {OPTION_MIC_125,"Gain 125%"},
+      {OPTION_MIC_150,"Gain 150%"},
+      {OPTION_MIC_175,"Gain 175%"},
+      {OPTION_MIC_200,"Gain 200%"},
+      {OPTION_EXIT,"Exit"},
+    }
+  },
+  {
+    MENU_CESSB,
+    "CESSB",
+    3U,
+    {
+      {OPTION_CESSB_ON,"On"},
+      {OPTION_CESSB_OFF,"Off"},
+      {OPTION_EXIT,"Exit"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"}
+    }
+  },
+  {
+    MENU_GAUSSIAN,
+    "Gaussian",
+    3U,
+    {
+      {OPTION_GAUSSIAN_ON,"On"},
+      {OPTION_GAUSSIAN_OFF,"Off"},
+      {OPTION_EXIT,"Exit"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"}
+    }
+  },
+  {
+    MENU_EXIT,
+    "Exit",
+    1U,
+    {
+      {OPTION_EXIT,"Exit"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"},
+      {OPTION_NONE,"None"}
+    }
+  }
+};
+
+#endif
